097eeb57c946edeb4e9fedf7c534de87f604722c
campovski <campovski@gmail.com>
2021-09-19 18:51:11 +0200

    SCIDASH-3: WIP backup

--- lib/core/ndarray.dart
+++ lib/core/ndarray.dart
@@ -1,3 +1,5 @@
+import 'package:scidash/core/complex.dart';
+
 class NDArray<T> {
   NDArray(List ndarray) : _els = ndarray {
     _dims = _calculateDimensions(ndarray).reversed.toList();
@@ -11,10 +13,16 @@ class NDArray<T> {
 
   Type get type => T;
 
+  NDArray slice(List<List<int>> ranges) {
+    assert(ranges.length == n,
+        'length of ranges does not equal number of dimensions');
+    throw UnimplementedError();
+  }
+
   List<int> _calculateDimensions(List ndarray) {
     if (ndarray.isEmpty) throw RangeError('ndarray is empty');
-    if (!(ndarray.first is List)) {
-      if (!(ndarray.first is T)) throw TypeError();
+    if (ndarray.first is! List) {
+      if (ndarray.first is! T) throw TypeError();
       return [ndarray.length];
     }
 
@@ -30,8 +38,67 @@ class NDArray<T> {
     return firstChildDimensions;
   }
 
+  void _traverseAndOperateOnElementWith(T Function(T) f, [List? l]) {
+    if (l == null) l = _els;
+
+    if (l.first is! List) {
+      for (int i = 0; i < l.length; i++) {
+        l[i] = f(l[i]);
+      }
+      return;
+    }
+
+    for (List child in l) _traverseAndOperateOnElementWith(f, child);
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (o is! NDArray) return false;
+
+    final NDArray other = o;
+    return other._els == this._els;
+  }
+
   @override
   String toString() {
     return 'NDArray<$T>($_els)';
+  }
+}
+
+class NDDoubleArray extends NDArray<double> {
+  NDDoubleArray(List ndarray) : super(ndarray);
+
+  NDDoubleArray scale(double x) {
+    _traverseAndOperateOnElementWith((double el) => el * x);
+    return this;
+  }
+
+  NDDoubleArray operator +(NDDoubleArray other) {
+    throw UnimplementedError();
+  }
+
+  NDDoubleArray operator -(NDDoubleArray other) {
+    throw UnimplementedError();
+  }
+
+  NDDoubleArray operator *(NDDoubleArray other) {
+    throw UnimplementedError();
+  }
+
+  NDDoubleArray operator /(NDDoubleArray other) {
+    throw UnimplementedError();
+  }
+
+  NDDoubleArray operator [](List<int> indices) {
+    throw UnimplementedError();
+  }
+
+  void operator []=(List<int> indices, double newValue) {
+    throw UnimplementedError();
+  }
+
+  @override
+  String toString() {
+    return 'NDDoubleArray($_els)';
   }
 }
--- test/core/ndarray_test.dart
+++ test/core/ndarray_test.dart
@@ -2,40 +2,109 @@ import 'package:scidash/core/ndarray.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('_calculateDimensions', () {
-    test('should throw range error if part of ndarray is empty', () {
-      final List<List<int>> l = [
-        [],
-        [1, 2, 3]
-      ];
-
-      expect(() => NDArray<int>(l), throwsRangeError);
-    });
-
-    test('should throw ArgumentError if child dimensions mismatch', () {
-      final List<List<int>> l = [
-        [2, 3],
-        [1, 2, 3]
-      ];
-
-      expect(() => NDArray<int>(l), throwsArgumentError);
-    });
-
-    test(
-      'should throw TypeError if supplied and required types do not match',
-      () {
+  group('NDArray', () {
+    group('_calculateDimensions', () {
+      test('should throw range error if part of ndarray is empty', () {
         final List<List<int>> l = [
-          [1, 2, 3],
+          [],
           [1, 2, 3]
         ];
 
-        expect(() => NDArray<String>(l), throwsA(isA<TypeError>()));
-      },
-    );
+        expect(() => NDArray<int>(l), throwsRangeError);
+      });
 
-    test(
-      'should allow NDArray construction and calculate dimensions correctly',
-      () {
+      test('should throw ArgumentError if child dimensions mismatch', () {
+        final List<List<int>> l = [
+          [2, 3],
+          [1, 2, 3]
+        ];
+
+        expect(() => NDArray<int>(l), throwsArgumentError);
+      });
+
+      test(
+        'should throw TypeError if supplied and required types do not match',
+        () {
+          final List<List<int>> l = [
+            [1, 2, 3],
+            [1, 2, 3]
+          ];
+
+          expect(() => NDArray<String>(l), throwsA(isA<TypeError>()));
+        },
+      );
+
+      test(
+        'should allow NDArray construction and calculate dimensions correctly',
+        () {
+          final List<List<List<int>>> l = [
+            [
+              [1, 1, 1, 1],
+              [1, 1, 1, 1],
+              [1, 1, 1, 1]
+            ],
+            [
+              [2, 2, 2, 2],
+              [2, 2, 2, 2],
+              [2, 2, 2, 2]
+            ]
+          ];
+
+          final NDArray<int> ndarray = NDArray<int>(l);
+          expect(ndarray.n, equals(3));
+          expect(ndarray.dimensions, equals([2, 3, 4]));
+        },
+      );
+    });
+
+    group('get dimensions', () {
+      test(
+        'should return dimensions of NDArray',
+        () {
+          final List<List<List<int>>> l = [
+            [
+              [1, 1, 1, 1],
+              [1, 1, 1, 1],
+              [1, 1, 1, 1]
+            ],
+            [
+              [2, 2, 2, 2],
+              [2, 2, 2, 2],
+              [2, 2, 2, 2]
+            ]
+          ];
+
+          final NDArray<int> ndarray = NDArray<int>(l);
+          expect(ndarray.dimensions, equals([2, 3, 4]));
+        },
+      );
+    });
+
+    group('get n', () {
+      test(
+        'should return number of axes of NDArray',
+        () {
+          final List<List<List<int>>> l = [
+            [
+              [1, 1, 1, 1],
+              [1, 1, 1, 1],
+              [1, 1, 1, 1]
+            ],
+            [
+              [2, 2, 2, 2],
+              [2, 2, 2, 2],
+              [2, 2, 2, 2]
+            ]
+          ];
+
+          final NDArray<int> ndarray = NDArray<int>(l);
+          expect(ndarray.n, equals(3));
+        },
+      );
+    });
+
+    group('get type', () {
+      test('should return the type of ndarray as provided by user', () {
         final List<List<List<int>>> l = [
           [
             [1, 1, 1, 1],
@@ -50,16 +119,13 @@ void main() {
         ];
 
         final NDArray<int> ndarray = NDArray<int>(l);
-        expect(ndarray.n, equals(3));
-        expect(ndarray.dimensions, equals([2, 3, 4]));
-      },
-    );
-  });
 
-  group('get dimensions', () {
-    test(
-      'should return dimensions of NDArray',
-      () {
+        expect(ndarray.type, equals(int));
+      });
+    });
+
+    group('toString', () {
+      test('should return desired string representation', () {
         final List<List<List<int>>> l = [
           [
             [1, 1, 1, 1],
@@ -74,77 +140,93 @@ void main() {
         ];
 
         final NDArray<int> ndarray = NDArray<int>(l);
-        expect(ndarray.dimensions, equals([2, 3, 4]));
-      },
-    );
-  });
-
-  group('get n', () {
-    test(
-      'should return number of axes of NDArray',
-      () {
-        final List<List<List<int>>> l = [
-          [
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-            [1, 1, 1, 1]
-          ],
-          [
-            [2, 2, 2, 2],
-            [2, 2, 2, 2],
-            [2, 2, 2, 2]
-          ]
-        ];
-
-        final NDArray<int> ndarray = NDArray<int>(l);
-        expect(ndarray.n, equals(3));
-      },
-    );
-  });
-
-  group('get type', () {
-    test('should return the type of ndarray as provided by user', () {
-      final List<List<List<int>>> l = [
-        [
-          [1, 1, 1, 1],
-          [1, 1, 1, 1],
-          [1, 1, 1, 1]
-        ],
-        [
-          [2, 2, 2, 2],
-          [2, 2, 2, 2],
-          [2, 2, 2, 2]
-        ]
-      ];
-
-      final NDArray<int> ndarray = NDArray<int>(l);
-
-      expect(ndarray.type, equals(int));
+        print(ndarray);
+        expect(
+          ndarray.toString(),
+          equals(
+              'NDArray<int>([[[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]], [[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]])'),
+        );
+      });
     });
   });
 
-  group('toString', () {
-    test('should return desired string representation', () {
-      final List<List<List<int>>> l = [
-        [
-          [1, 1, 1, 1],
-          [1, 1, 1, 1],
-          [1, 1, 1, 1]
-        ],
-        [
-          [2, 2, 2, 2],
-          [2, 2, 2, 2],
-          [2, 2, 2, 2]
-        ]
-      ];
+  group('NDDoubleArray', () {
+    group('NDArray extension', () {
+      test('should succeed if T is double', () {
+        final List<List<List<double>>> l = [
+          [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+          ],
+          [
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2]
+          ]
+        ];
 
-      final NDArray<int> ndarray = NDArray<int>(l);
-      print(ndarray);
-      expect(
-        ndarray.toString(),
-        equals(
-            'NDArray<int>([[[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]], [[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]])'),
-      );
+        final NDDoubleArray _ = NDDoubleArray(l);
+      });
+    });
+
+    group('scale', () {
+      test('should correctly scale the ndarray', () {
+        final List<List<List<double>>> l = [
+          [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+          ],
+          [
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2]
+          ]
+        ];
+        final List<List<List<double>>> lExpected = [
+          [
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2]
+          ],
+          [
+            [4, 4, 4, 4],
+            [4, 4, 4, 4],
+            [4, 4, 4, 4]
+          ]
+        ];
+
+        final NDDoubleArray ndda = NDDoubleArray(l).scale(2);
+        final NDDoubleArray nddaExpected = NDDoubleArray(lExpected);
+
+        expect(ndda, equals(nddaExpected));
+      });
+    });
+
+    group('toString', () {
+      test('should return desired string representation', () {
+        final List<List<List<int>>> l = [
+          [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+          ],
+          [
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2]
+          ]
+        ];
+
+        final NDArray<int> ndarray = NDArray<int>(l);
+        print(ndarray);
+        expect(
+          ndarray.toString(),
+          equals(
+              'NDArray<int>([[[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]], [[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]])'),
+        );
+      });
     });
   });
 }
